237d98687820a49411ab44991aea83f46f61ef28
bachTuan <bachtuananh16@gmail.com>
2022-04-24 03:10:04 +0700

    make it better

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = async function(req, res, next) {
     try {
@@ -6,21 +6,21 @@ module.exports = async function(req, res, next) {
             return res.status(404).json({ msg: "not file upload" });
         }
         const file = req.file;
-        if (file.size > 1024 * 1024) {
-            removeImg("public/uploads/" + file.filename);
-            return res.status(404).json({ msg: "Size to large" });
-        }
-        if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+        // if (file.size > 1024 * 1024) {
+        //     removeImg("public/uploads/" + file.filename);
+        //     return res.status(404).json({ msg: "Size to large" });
+        // }
+        if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
             removeImg("public/uploads/" + file.filename);
             return res.status(404).json({ msg: "file is not format" });
         }
-        next()
+        next();
     } catch (err) {
         return res.status(404).json({ msg: err.message });
     }
-}
+};
 const removeImg = function(patchFile) {
-    fs.unlink(patchFile, err => {
+    fs.unlink(patchFile, (err) => {
         if (err) throw err;
-    })
-}
+    });
+};
